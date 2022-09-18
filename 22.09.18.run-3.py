8d5a6026ee8098c7bc5b8aa396465fbf36c2eacd
hoyul1845 <hoyul1845@naver.com>
2022-09-18 11:29:36 +0900

    22.09.18.달려라 파이썬 9강

---
+++ 22.09.18.run-3.py
@@ -0,0 +1,16 @@
+# 입력(예:input()과출력(예:print())
+# 함수:특정기능[작업]을 한뒤 그 결과 값을 알려주는 것
+# 함수 사용법:함수를 쓰고 괄호 열고닫고 괄호에 입력
+# print() :출력함수(예:print('안녕',name)).사용법:print쓰고 괄호 안에 출력 하고 싶은 것을 쓴다.
+
+'''
+name = input("너의 이름:")
+print('안녕',name,)
+
+number1 = input('첫 번째 수:')
+number2 = input('두 번째 수:')
+total = int(number1) + int(number2)
+print(number1,number2,total)'''
+
+animal = input("내가 좋아하는 동물:")
+print('내가 좋아하는 동물은',animal ,"입니다.")
