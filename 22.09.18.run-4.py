640d2f504b38dde67e222d2b4f2f1c1b3b4631ff
hoyul1845 <hoyul1845@naver.com>
2022-09-18 12:47:10 +0900

    22.09.18.달려라 파이썬 10강

---
+++ 22.09.18.run-4.py
@@ -0,0 +1,16 @@
+# int :정수, float:실수(소수)
+# Python 곱하기 = *
+# %는 나머지 구하기
+apple = 30
+orange = 10
+banana = 25
+student = 7
+
+fruit = apple + orange + banana
+print(fruit)
+cc = apple - orange
+print(cc)
+banena = banana // student
+ss = banana % student
+print(banena)
+print(ss)
