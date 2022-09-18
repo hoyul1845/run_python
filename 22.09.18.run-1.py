fe282e7646ff3f1aa9be1d8dbd091c16e1445328
hoyul1845 <hoyul1845@naver.com>
2022-09-18 10:19:40 +0900

    22.09.18.달려라 파이썬 7강

---
+++ 22.09.18.run-1.py
@@ -0,0 +1,17 @@
+# 변수 = 저장 할 데이타,데이타이름 또는 데이타가 저장되는 장소
+# 변수라 부루는 이유:변하는 수에서 변수다.또 변수에 저장된 값은 변할수가 있어서기도 하다.
+# 변수명 규칙1 문자숫자 = 숫자가 먼저나오면 안됨
+# 변수명 규칙2 특별 문자 = 특별 문자(예:!@#$%^&*())같은 것은 안되지만 _는 예외다.
+number1 = 100
+print(number1)
+# 변수명 규칙3 예약어 사용 금지 = (예약어:중요한 이름(예:False,class,from,or,None,continue,global,pass,True,def,if,raise,and,del,import,return,as,elif,in,try등등)
+# key world로 변수를 정하면 오류가 난다.(예:class = 3)
+# 또 변수 이름을 의미있게 짓는다.(예:color_name = '빨강')
+# 주의사항: name과NAME은 다르다.또 띄어쓰기,들여쓰기도 주의해야 한다.
+#number 1 = 100(x)
+# number1 = 100(x)
+name = '라이언'
+fruit = '사과'
+animal = '개'
+icecream_31 = 3
+if_name = 100
