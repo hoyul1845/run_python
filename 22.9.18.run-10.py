ec6f3a1a87d0499938abeb222a460e9c70b8fdd3
hoyul1845 <hoyul1845@naver.com>
2022-09-18 19:59:57 +0900

    22.09.18.달려라 파이썬 16강

---
+++ 22.9.18.run-10.py
@@ -0,0 +1,13 @@
+# 연습문제
+# 친구들이 좋아하는 스포츠 리스트
+sports = ["축구","수영","야구","농구","축구","댄스"]
+print(sports)
+
+sports[3] = "달리기"
+print(sports)
+
+del sports[4]
+print(sports)
+
+count = len(sports)
+print(count)
