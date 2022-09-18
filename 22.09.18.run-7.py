32703454f261d6591bd2b85d15d8329827dabea1
hoyul1845 <hoyul1845@naver.com>
2022-09-18 18:14:47 +0900

    22.09.18.달려라 파이썬 13강

---
+++ 22.09.18.run-7.py
@@ -0,0 +1,20 @@
+# bool: 참과거짓(True,False)(예:light_on = True)
+# bool: 만약숫자가 있으면 True 없으면 False 만약문자가 있으면 True 없으면 False 만약리스트에 뭐가 들어 있으면 True 없으면 False 또 만약None이 있으면 False 없어도 False를 출력한다.
+# 비교 연산자:>,<,>=,<=,==,!=(예:7>3,7<3(출력:1:True,2:False))
+#비교 연산자
+#number = int(input("수를 입력하세요.:"))
+
+#print("10보다 큽니다.",number > 10)
+#print("10보다 작습니다.",number < 10)
+#print("10과 같습니다.",number == 10)
+#print("10이 아닙니다.",number != 10)
+
+# 문자 비교하기
+#name = input("이름을 입력하세요.:")
+#print("이름은 라이온 입니다.",name == "라이온")
+
+#1False
+#2True
+#3True
+#4False
+#5False
