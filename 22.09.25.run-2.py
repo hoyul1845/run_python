7928cac646d9a5ebaa6128952174d0625b159312
hoyul1845 <hoyul1845@naver.com>
2022-09-25 15:20:52 +0900

    22.09.25.달려라 파이썬 98강

---
+++ 22.09.25.run-2.py
@@ -0,0 +1,14 @@
+student = ["유재석","지석진","김종국","양세형","하하","송지효","이광수","전소민","양세찬"]
+print(student)
+
+student.remove("양세형")
+print(student)
+
+student.append("송지효")
+print(student)
+
+count = student.count("송지효")
+print(count)
+
+student.sort()
+print(student)
