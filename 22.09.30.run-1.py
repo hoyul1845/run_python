8c69848331a98d348786edf09719fbbfa97cf3db
hoyul1845 <hoyul1845@naver.com>
2022-09-30 21:47:23 +0900

    22.09.30.달려라 파이썬 29강

---
+++ 22.09.30.run-1.py
@@ -0,0 +1,12 @@
+# range:수열 생성 함수
+
+# range함수를 써서 수열 만들기
+'''ret = range(20,0,-2)
+number = list(ret)
+print(number)
+
+for i in number:
+    print(i)'''
+    
+for letter in "Python":
+    print(letter)
