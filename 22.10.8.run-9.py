6b8886b3f0e9302fe97362164d1124a7354b86cd
hoyul1845 <hoyul1845@naver.com>
2022-10-08 13:34:26 +0900

    22.10.8.달려라 파이썬 9강

---
+++ 22.10.8.run-9.py
@@ -0,0 +1,11 @@
+
+'''name = input("너의 이름:")
+print('안녕',name,)
+
+number1 = input('첫 번째 수:')
+number2 = input('두 번째 수:')
+total = int(number1) + int(number2)
+print(number1,"+",number2,"=",total)'''
+
+animal = input('좋아하는 동물은 무엇이니?')
+print("내가 좋아하는 동물은",animal,"입니다.")
