02c61ad6e079c742dc1e69f103e447d77d72d81a
hoyul1845 <hoyul1845@naver.com>
2022-10-08 13:07:52 +0900

    22.10.8.달려라 파이썬 8강

---
+++ 22.10.8.run-8.py
@@ -0,0 +1,15 @@
+#데이타의 종류는 다양함
+name = '전정국'
+rope = 200 # 줄넘기 횟수
+rope_time = 90 # 90초
+rope_pass = True
+
+print(name)
+print(rope)
+print(name,rope,rope_time,rope_pass)
+
+rope = 56
+day = 23.5
+rain = False
+mycountry = '대한민국'
+rainbowlist = ['빨','주','노','초','파','남','보']
